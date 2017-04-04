29f4dfdd2c3dd8dca290f80222c01e20d6c25e60
numi <takatsho@gmail.com>
2017-04-04 16:41:40 +0200

    Added vertical animation and changed the profile photo

--- logic.js
+++ logic.js
@@ -1,10 +1,13 @@
 var tempOpacity = 0;
+var padding = 0;
 var animate = null;
 
 function fade(){
 	document.getElementById("content").style.opacity = tempOpacity;
+	document.getElementById("content").style.paddingTop = padding+"%";
 	if(tempOpacity < 1){
 		tempOpacity += 0.015;
+		padding += 0.03;
 		if(tempOpacity > 1){
 			tempOpacity = 1;
 		}
