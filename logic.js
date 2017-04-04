899c081ea8b58dcc9be26e2d8687b37bb3dbe207
numi <takatsho@gmail.com>
2017-04-04 18:59:25 +0200

    Added animation to the credits bar and edited the profile photo for more brightness

--- logic.js
+++ logic.js
@@ -5,9 +5,10 @@ var animate = null;
 function fade(){
 	document.getElementById("content").style.opacity = tempOpacity;
 	document.getElementById("content").style.paddingTop = padding+"%";
+	document.getElementById("content").style.paddingBottom = padding+"%";
 	if(tempOpacity < 1){
 		tempOpacity += 0.015;
-		padding += 0.03;
+		padding += 0.02;
 		if(tempOpacity > 1){
 			tempOpacity = 1;
 		}
